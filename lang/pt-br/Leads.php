1e1c2eef5e49639ae4aefcf78b70a001896071a8
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-10-14 21:31:47 -0300

    feat: ajustado mensagem de erro para lead já registrado em nossa base de dados

--- lang/pt-br/Leads.php
+++ lang/pt-br/Leads.php
@@ -4,7 +4,8 @@ return [
     'success' => 'Seu interesse  no produto  foi registrado com sucesso, nossa ' .
         'equipe entrará em contato em breve para mais detalhes, você agora esta em ' .
         'nossa fila de espera, assim que o produto for lançado você será notificado.',
-    'email.unique' => 'O email informado já está cadastrado em nossa base de dados.',
+    'email.unique' => 'O email informado já está cadastrado em nossa base de dados. ' .
+        'Logo nossa equipe entrará em contato com você para mais detalhes.',
     'email.required' => 'O campo email é obrigatório.',
     'name.required' => 'O campo nome é obrigatório.',
     'experience_level.required' => 'O campo nível de experiência é obrigatório.',
