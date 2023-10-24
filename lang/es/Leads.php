29c153cd4f40f469100a9e8737eb53f003f4cd93
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-10-23 21:34:27 -0300

    feat: adicionado todas as linguagens que daremos suporte para a aplicação

---
+++ lang/es/Leads.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'success' => 'Su interés en el producto ha sido registrado con éxito, ' .
+        'nuestro equipo se pondrá en contacto contigo en breve para más detalles, ahora estás en nuestra' .
+        'lista de espera, tan pronto como se lance el producto serás notificado.',
+    'email.unique' => 'El correo electrónico proporcionado ya está registrado en nuestra base de datos. ' .
+        'Nuestro equipo se pondrá en contacto contigo en breve para más detalles.',
+    'email.required' => 'El campo de correo electrónico es obligatorio.',
+    'name.required' => 'El campo de nombre es obligatorio.',
+    'experience_level.required' => 'El campo de nivel de experiencia es obligatorio.',
+    'message.string' => 'El campo de mensaje debe ser una cadena.',
+    'emailConfirmed' => 'Tu correo electrónico ha sido confirmado con éxito.',
+];
---
+++ lang/it/Leads.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'success' => 'Il tuo interesse per il prodotto è stato registrato con successo, ' .
+        'il nostro team ti contatterà a breve per ulteriori dettagli, ora sei nella nostra' .
+        'lista d\'attesa, non appena il prodotto sarà lanciato sarai notificato.',
+    'email.unique' => 'L\'email fornita è già registrata nel nostro database. ' .
+        'Il nostro team ti contatterà a breve per ulteriori dettagli.',
+    'email.required' => 'Il campo email è obbligatorio.',
+    'name.required' => 'Il campo nome è obbligatorio.',
+    'experience_level.required' => 'Il campo livello di esperienza è obbligatorio.',
+    'message.string' => 'Il campo messaggio deve essere una stringa.',
+    'emailConfirmed' => 'La tua email è stata confermata con successo.',
+];
