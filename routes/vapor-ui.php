9df524add381beaa9601d87fdc695964d54c00f6
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-11-09 23:54:25 -0300

    feat(VaporUi): adicionando opção de login no Vapor UI (#12)
    * feat: criado autenticação com GitHub para tela do Laravel VaporUI
    
    * chore: ajuste variável de autenticação GitHub

---
+++ routes/vapor-ui.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginGitHubController;
+use Illuminate\Support\Facades\Route;
+use Laravel\VaporUi\VaporUi;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/vapor/login', function () {
+    return view('welcome-vapor-ui');
+})->name('welcome-vapor-ui');
+
+Route::get('/logout', [LoginGitHubController::class, 'logout'])->name('logout');
+
+Route::get('/auth/github/redirect', [LoginGitHubController::class, 'githubRedirect'])->name('github.login');
+
+Route::get('/auth/github/callback', [LoginGitHubController::class, 'githubCallback']);
