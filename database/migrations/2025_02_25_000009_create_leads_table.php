770d2ef488041e8874240716821f8309ef1390e5
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-02-25 22:24:51 -0300

    feat(database): substituir migração da tabela de leads com criação condicional e suporte a auto incremento e índice único

---
+++ database/migrations/2025_02_25_000009_create_leads_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (!Schema::hasTable('leads')) {
+            Schema::create('leads', function (Blueprint $table) {
+                $table->bigInteger('id'); // AUTO_INCREMENT será tratado na segunda migration
+                $table->string('name');
+                $table->string('email');
+                $table->timestamp('email_verified_at')->nullable();
+                $table->enum(
+                    'status',
+                    [
+                        'new',
+                        'contacted',
+                        'converted',
+                        'unqualified',
+                        'qualified',
+                        'bad_email',
+                        'spam',
+                    ]
+                )->default('new');
+                $table->enum(
+                    'experience_level',
+                    [
+                        'beginner',
+                        'amateur',
+                        'student',
+                        'college',
+                        'semi-professional',
+                        'professional',
+                        'intermediate',
+                        'coach',
+                        'instructor',
+                        'other',
+                    ]
+                )->default('beginner');
+                $table->text('message');
+                $table->timestamp('created_at')->nullable();
+                $table->timestamp('updated_at')->nullable();
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
---
+++ database/migrations/2025_02_25_000010_modify_leads_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (
+            Schema::hasTable('leads') &&
+            Schema::hasColumn('leads', 'id') &&
+            !hasAutoIncrement('leads')
+        ) {
+            DB::statement(
+                'ALTER TABLE leads MODIFY id BIGINT UNSIGNED AUTO_INCREMENT'
+            );
+        }
+
+        if (Schema::hasTable('leads') && !hasIndexExist('leads', 'leads_email_unique')) {
+            Schema::table('leads', function (Blueprint $table) {
+                $table->unique('email', 'leads_email_unique');
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        if (
+            Schema::hasTable('leads') &&
+            Schema::hasColumn('leads', 'id') &&
+            hasAutoIncrement('leads')
+        ) {
+            DB::statement(
+                'ALTER TABLE leads MODIFY id BIGINT UNSIGNED NOT NULL'
+            );
+        }
+
+        if (Schema::hasTable('leads') && hasIndexExist('leads', 'leads_email_unique')) {
+            Schema::table('leads', function (Blueprint $table) {
+                $table->dropUnique('leads_email_unique');
+            });
+        }
+    }
+};
